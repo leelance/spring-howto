a4b0121402cfafd620762e633f922f48d7887f82
lihao <li6151770@126.com>
2022-01-20 21:36:04 +0800

    add sharding

---
+++ sharding-howto/src/main/java/com/lance/sharding/ShardingHowtoApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingHowtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingHowtoApplication.class, args);
+	}
+
+}
---
+++ sharding-howto/src/test/java/com/lance/sharding/ShardingHowtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingHowtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
