f5a4c791187ee1c47288ae805b5cfe30324dfe3e
lance <li6151770@126.com>
2022-02-18 11:31:40 +0800

    add sharding-jdbc read write

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-mybatis-rw/src/main/java/com/lance/sharding/rw/MybatisRwApplication.java
@@ -0,0 +1,19 @@
+package com.lance.sharding.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * sharding jdbc mybatis read write
+ *
+ * @author lance
+ * @date 2022/2/18 11:28
+ */
+@SpringBootApplication
+public class MybatisRwApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MybatisRwApplication.class, args);
+  }
+
+}
---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-mybatis-rw/src/test/java/com/lance/sharding/rw/MybatisRwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.rw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisRwApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
