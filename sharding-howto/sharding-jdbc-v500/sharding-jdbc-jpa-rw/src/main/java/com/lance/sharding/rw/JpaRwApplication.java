60679d5a302b217f4fc2d808a1b968510c58ac1c
lance <li6151770@126.com>
2022-02-19 11:45:14 +0800

    add sharding-jdbc rw jpa

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-rw/src/main/java/com/lance/sharding/rw/JpaRwApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRwApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaRwApplication.class, args);
+  }
+
+}
---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-rw/src/test/java/com/lance/sharding/rw/JpaRwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.rw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRwApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
