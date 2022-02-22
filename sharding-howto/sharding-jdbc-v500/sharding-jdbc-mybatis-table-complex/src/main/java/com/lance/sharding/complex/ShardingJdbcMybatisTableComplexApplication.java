9ac032933948705ed8f86a1c6dbae9c02bbef48b
lihao <li6151770@126.com>
2022-02-22 23:36:26 +0800

    add table complex mybatis

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-mybatis-table-complex/src/main/java/com/lance/sharding/complex/ShardingJdbcMybatisTableComplexApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.complex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcMybatisTableComplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcMybatisTableComplexApplication.class, args);
+	}
+
+}
---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-mybatis-table-complex/src/test/java/com/lance/sharding/complex/ShardingJdbcMybatisTableComplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.complex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingJdbcMybatisTableComplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
