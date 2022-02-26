94449301c85305c1bda3b401c73bc2c27e99b154
lance <li6151770@126.com>
2022-02-27 00:35:06 +0800

    add sharding jpa table auto interval

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-table-auto-interval/src/main/java/com/lance/sharding/interval/ShardingJdbcJpaTableAutoIntervalApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.interval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcJpaTableAutoIntervalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShardingJdbcJpaTableAutoIntervalApplication.class, args);
+  }
+
+}
