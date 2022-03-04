3e310c54fc018ecd621af875858791d93d77b240
lance <li6151770@126.com>
2022-03-04 14:18:37 +0800

    add module jpa volume range

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-table-volume-range/src/main/java/com/lance/sharding/volume/ShardingJdbcJpaTableVolumeRangeApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.volume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcJpaTableVolumeRangeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShardingJdbcJpaTableVolumeRangeApplication.class, args);
+  }
+
+}
