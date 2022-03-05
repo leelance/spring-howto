15b93464730ce36b0f0dd35b25760c38485bce89
lance <li6151770@126.com>
2022-03-05 23:27:09 +0800

    update application

--- sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-encrypt/src/main/java/com/lance/sharding/encrypt/JpaEncryptApplication.java
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-encrypt/src/main/java/com/lance/sharding/encrypt/JpaEncryptApplication.java
@@ -3,6 +3,12 @@ package com.lance.sharding.encrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * jpa column encrypt
+ *
+ * @author lance
+ * @date 2022/3/5 23:26
+ */
 @SpringBootApplication
 public class JpaEncryptApplication {
 
