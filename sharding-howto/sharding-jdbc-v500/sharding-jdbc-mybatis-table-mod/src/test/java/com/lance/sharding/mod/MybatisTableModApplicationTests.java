762986d265c5252413403d81cb04e3f9542ca5e6
lance <li6151770@126.com>
2022-03-05 18:54:50 +0800

    add mybatis table mod module

---
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-mybatis-table-mod/src/test/java/com/lance/sharding/mod/MybatisTableModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.sharding.mod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisTableModApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
