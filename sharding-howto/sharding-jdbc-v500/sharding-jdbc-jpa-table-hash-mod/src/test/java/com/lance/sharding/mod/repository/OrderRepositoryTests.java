d1e5de03bd402a5569695a5e1f1ca9082110c4cc
lance <li6151770@126.com>
2022-03-05 16:42:34 +0800

    add test

--- sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-table-hash-mod/src/test/java/com/lance/sharding/mod/repository/OrderRepositoryTests.java
+++ sharding-howto/sharding-jdbc-v500/sharding-jdbc-jpa-table-hash-mod/src/test/java/com/lance/sharding/mod/repository/OrderRepositoryTests.java
@@ -1,6 +1,6 @@
-package com.lance.sharding.mod.mapper;
+package com.lance.sharding.mod.repository;
 
-import com.lance.sharding.mod.domain.Order;
+import com.lance.sharding.mod.entity.OrderEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.Disabled;
@@ -16,47 +16,58 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 /**
- * order[0..5], hash mod
+ * order volume range repository
  *
  * @author lance
- * @date 2022/3/5 16:24
+ * @date 2022/3/4 14:22
  */
 @Slf4j
 @SpringBootTest
-class OrderMapperTests {
+class OrderRepositoryTests {
   private final static String[] CITIES = {"shanghai", "beijing"};
   /**
    * -120000, 0, 18, 20000, 40000, 50000, 60000, 64000, 80000, 99000
    */
   private final static long[] PRICES = {-120000, 0, 18, 20000, 40000, 50000, 60000, 64000, 80000, 99000};
   @Autowired
-  private OrderMapper orderMapper;
+  private OrderRepository orderRepository;
 
   @Test
   @Disabled
   void save() {
     ThreadLocalRandom random = ThreadLocalRandom.current();
-
     Date[] dates = create();
+
     IntStream.range(0, 20).forEach(i -> {
-      Order order = new Order();
+      OrderEntity order = new OrderEntity();
       order.setOrderId(System.nanoTime() + i);
       order.setPrice(PRICES[i % PRICES.length]);
       order.setAddressId(i);
       order.setCity(CITIES[i % 2]);
       order.setUserId(Math.abs(random.nextInt()));
+      order.setIntervalTime(dates[i % 6]);
       order.setCreator("user.0" + i);
-      order.setIntervalTime(dates[i % 5]);
+      order.setCreateTime(new Date());
       order.setUpdater(order.getCreator());
-      log.info("====>{}", order);
-      orderMapper.save(order);
+      order.setUpdateTime(order.getCreateTime());
+      orderRepository.save(order);
     });
   }
 
   @Test
   @Disabled
-  void findAll() {
-    List<Order> list = orderMapper.findAllAtPrice(60000);
+  void findOne() {
+    long orderId = 335827518705018L;
+    OrderEntity orderEntity = orderRepository.findByOrderIdAndPrice(orderId, 80000L);
+    log.info("===>{}", orderEntity);
+  }
+
+  @Test
+  @Disabled
+  void findByPrice() {
+    List<OrderEntity> list = orderRepository.findByPrice(50000L);
+
+    log.info("===>{}", !list.isEmpty() ? list.size() : 0);
     log.info("===>{}", list);
   }
 
