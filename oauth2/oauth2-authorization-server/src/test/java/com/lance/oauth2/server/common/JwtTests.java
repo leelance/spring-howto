a30dbbe052fef70992e86a77a974db1ac59b3229
lance <li6151770@126.com>
2022-03-17 16:59:11 +0800

    ignore test

--- oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/common/JwtTests.java
+++ oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/common/JwtTests.java
@@ -1,17 +1,31 @@
 package com.lance.oauth2.server.common;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.util.CollectionUtils;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * jwt
@@ -23,26 +37,122 @@ import java.util.Map;
 class JwtTests {
 
 
-	@Test
-	void encoder() {
-		//Jwt jwt = Jwt.withTokenValue("33").build();
-		String clientSecret = "abc123456789";
-		SecretKeySpec secretKeySpec = new SecretKeySpec(clientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-		//Jwt jwt = Jwt.withTokenValue(clientSecret).build();
-		JWKSource<SecurityContext> jwkSource = new ImmutableSecret<>(secretKeySpec);
-		NimbusJwtEncoder encoder = new NimbusJwtEncoder(jwkSource);
-		Map<String, Object> claims = new HashMap<>(8);
-		claims.put("secret", clientSecret);
+  @Test
+  @Disabled
+  void encoder() {
+    //Jwt jwt = Jwt.withTokenValue("33").build();
+    String clientSecret = "abc123456789";
+    SecretKeySpec secretKeySpec = new SecretKeySpec(clientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    //Jwt jwt = Jwt.withTokenValue(clientSecret).build();
+    JWKSource<SecurityContext> jwkSource = new ImmutableSecret<>(secretKeySpec);
+    NimbusJwtEncoder encoder = new NimbusJwtEncoder(jwkSource);
+    Map<String, Object> claims = new HashMap<>(8);
+    claims.put("secret", clientSecret);
 
-		JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
-		JwtClaimsSet claimsSet = JwtClaimsSet.builder().claims(c -> c.putAll(claims)).build();
-		JwtEncoderParameters parameters = JwtEncoderParameters.from(jwsHeader, claimsSet);
-		Jwt jwtEncode = encoder.encode(parameters);
-		log.info("===>encoder: {}", jwtEncode.getClaims());
+    JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
+    JwtClaimsSet claimsSet = JwtClaimsSet.builder().claims(c -> c.putAll(claims)).build();
+    JwtEncoderParameters parameters = JwtEncoderParameters.from(jwsHeader, claimsSet);
+    Jwt jwtEncode = encoder.encode(parameters);
+    log.info("===>encoder: {}", jwtEncode.getClaims());
 
-		String token = encoder.toString();
-		NimbusJwtDecoder decoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
-		Jwt jwtDecode = decoder.decode(token);
-		log.info("===>decoder: {}", jwtDecode.getClaims());
-	}
+    String token = encoder.toString();
+    NimbusJwtDecoder decoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+    Jwt jwtDecode = decoder.decode(token);
+    log.info("===>decoder: {}", jwtDecode.getClaims());
+  }
+
+  @Test
+  @Disabled
+  void signer() throws Exception {
+    // Generate random 256-bit (32-byte) shared secret
+    SecureRandom random = new SecureRandom();
+    byte[] sharedSecret = new byte[32];
+    random.nextBytes(sharedSecret);
+
+    // Create HMAC signer
+    JWSSigner signer = new MACSigner(sharedSecret);
+
+    // Prepare JWT with claims set
+    JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+        .subject("alice")
+        .issuer("https://c2id.com")
+        .claim("username", "19000000000")
+        .claim("password", "abc@123")
+        .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+        .build();
+
+    SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+    // Apply the HMAC protection
+    signedJWT.sign(signer);
+
+    // Serialize to compact form, produces something like
+    // eyJhbGciOiJIUzI1NiJ9.SGVsbG8sIHdvcmxkIQ.onO9Ihudz3WkiauDO2Uhyuz0Y18UASXlSc1eS0NkWyA
+    String s = signedJWT.serialize();
+    log.info("===>sign: {}", s);
+
+    // On the consumer side, parse the JWS and verify its HMAC
+    signedJWT = SignedJWT.parse(s);
+    log.info("===>Claim: {}", signedJWT.getJWTClaimsSet().getClaims());
+    JWSVerifier verifier = new MACVerifier(sharedSecret);
+
+    Assertions.assertTrue(signedJWT.verify(verifier));
+
+    // Retrieve / verify the JWT claims according to the app requirements
+    Assertions.assertEquals("alice", signedJWT.getJWTClaimsSet().getSubject());
+    Assertions.assertEquals("https://c2id.com", signedJWT.getJWTClaimsSet().getIssuer());
+    Assertions.assertTrue(new Date().before(signedJWT.getJWTClaimsSet().getExpirationTime()));
+  }
+
+  /**
+   * 对client_security进行jwt进行签名, jose签名
+   */
+  @Test
+  @Disabled
+  void encode() throws Exception {
+    String clientId = "8000000014";
+    String clientSecret = "a5a0ddb27da70b41d31954d0c51419d8";
+    SecretKeySpec secretKeySpec = new SecretKeySpec(clientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+
+    JWSSigner signer = new MACSigner(secretKeySpec);
+    JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+        .subject(clientId)
+        .issuer(clientId)
+        .claim("username", "19000000000")
+        .claim("password", "abc@123")
+        .audience("http://auth-server:9000")
+        .expirationTime(new Date(new Date().getTime() + 60 * 60 * 60 * 1000))
+        .build();
+
+    SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+    signedJWT.sign(signer);
+
+    String token = signedJWT.serialize();
+    log.info("===>sign: {}", token);
+
+    NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+    jwtDecoder.setJwtValidator(createJwtValidator(clientId));
+    Jwt jwtDecode = jwtDecoder.decode(token);
+
+    log.info("===>{}", jwtDecode.getClaims());
+  }
+
+  private OAuth2TokenValidator<Jwt> createJwtValidator(String clientId) {
+    return new DelegatingOAuth2TokenValidator<>(
+        new JwtClaimValidator<>(JwtClaimNames.ISS, clientId::equals),
+        new JwtClaimValidator<>(JwtClaimNames.SUB, clientId::equals),
+        new JwtClaimValidator<>(JwtClaimNames.AUD, containsProviderAudience()),
+        new JwtClaimValidator<>(JwtClaimNames.EXP, Objects::nonNull),
+        new JwtTimestampValidator()
+    );
+  }
+
+  private Predicate<List<String>> containsProviderAudience() {
+    return (audienceClaim) -> {
+      if (CollectionUtils.isEmpty(audienceClaim)) {
+        return false;
+      }
+      return true;
+    };
+  }
 }
--- oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/repository/RegisteredClientRepositoryTests.java
+++ oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/repository/RegisteredClientRepositoryTests.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
@@ -30,95 +29,89 @@ import java.util.UUID;
 @Slf4j
 @SpringBootTest
 class RegisteredClientRepositoryTests {
-	@Autowired
-	private RegisteredClientRepository registeredClientRepository;
+  @Autowired
+  private RegisteredClientRepository registeredClientRepository;
 
-	@Test
-	@Disabled
-	void findByClientId() {
-		String clientId = "8000000010";
-		RegisteredClient client = registeredClientRepository.findByClientId(clientId);
+  @Test
+  @Disabled
+  void findByClientId() {
+    String clientId = "8000000010";
+    RegisteredClient client = registeredClientRepository.findByClientId(clientId);
 
-		log.info("===>{}", JsonUtils.toJsonString(client));
-	}
+    log.info("===>{}", JsonUtils.toJsonString(client));
+  }
 
-	@Test
-	@Disabled
-	void findById() {
-		String id = "833cec50-fc11-4488-b29c-d3bb7fe7da98";
-		RegisteredClient client = registeredClientRepository.findById(id);
+  @Test
+  @Disabled
+  void findById() {
+    String id = "833cec50-fc11-4488-b29c-d3bb7fe7da98";
+    RegisteredClient client = registeredClientRepository.findById(id);
 
-		log.info("===>{}", JsonUtils.toJsonString(client));
-	}
+    log.info("===>{}", JsonUtils.toJsonString(client));
+  }
 
-	@Test
-	@Disabled
-	void saveBase() {
-		String id = UUID.randomUUID().toString().replaceAll("-", "");
+  @Test
+  @Disabled
+  void saveBase() {
+    String id = UUID.randomUUID().toString().replaceAll("-", "");
 
-		TokenSettings tokenSettings = TokenSettings.builder()
-				.reuseRefreshTokens(true)
-				.refreshTokenTimeToLive(Duration.ofDays(7))
-				.accessTokenTimeToLive(Duration.ofHours(8))
-				.idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
-				.reuseRefreshTokens(false)
-				.build();
+    TokenSettings tokenSettings = TokenSettings.builder()
+        .reuseRefreshTokens(true)
+        .refreshTokenTimeToLive(Duration.ofDays(7))
+        .accessTokenTimeToLive(Duration.ofHours(8))
+        .idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
+        .reuseRefreshTokens(false)
+        .build();
 
-		RegisteredClient client = RegisteredClient.withId(id)
-				.clientId("8000000013")
-				.clientIdIssuedAt(Instant.now())
-				.clientSecret("{noop}secret")
-				.clientSecretExpiresAt(Instant.now().plus(Period.ofDays(20)))
-				.clientName("Client credentials client_secret_basic有限公司")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.scope("server")
-				.tokenSettings(tokenSettings)
-				.build();
-		registeredClientRepository.save(client);
+    RegisteredClient client = RegisteredClient.withId(id)
+        .clientId("8000000013")
+        .clientIdIssuedAt(Instant.now())
+        .clientSecret("{noop}secret")
+        .clientSecretExpiresAt(Instant.now().plus(Period.ofDays(20)))
+        .clientName("Client credentials client_secret_basic有限公司")
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+        .scope("server")
+        .tokenSettings(tokenSettings)
+        .build();
+    registeredClientRepository.save(client);
 
-		log.info("===>{}", JsonUtils.toJsonString(client));
-	}
+    log.info("===>{}", JsonUtils.toJsonString(client));
+  }
 
-	@Test
-	@Disabled
-	void saveJwt() {
-		String id = UUID.randomUUID().toString().replaceAll("-", "");
+  @Test
+  @Disabled
+  void saveJwt() {
+    String id = UUID.randomUUID().toString().replaceAll("-", "");
 
-		TokenSettings tokenSettings = TokenSettings.builder()
-				.reuseRefreshTokens(true)
-				.refreshTokenTimeToLive(Duration.ofDays(7))
-				.accessTokenTimeToLive(Duration.ofHours(8))
-				.idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
-				.reuseRefreshTokens(false)
-				.build();
+    TokenSettings tokenSettings = TokenSettings.builder()
+        .reuseRefreshTokens(true)
+        .refreshTokenTimeToLive(Duration.ofDays(7))
+        .accessTokenTimeToLive(Duration.ofHours(8))
+        .idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
+        .reuseRefreshTokens(false)
+        .build();
 
-		//Jwt jwt = Jwt.withTokenValue("33").build();
-		String clientSecret = "abc123456789";
-		//SecretKeySpec secretKeySpec = new SecretKeySpec(clientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-		//Jwt jwt = Jwt.withTokenValue(clientSecret).build();
-		NimbusJwtEncoder encoder = new NimbusJwtEncoder(null);
+    ClientSettings clientSettings = ClientSettings.builder()
+        .tokenEndpointAuthenticationSigningAlgorithm(MacAlgorithm.HS256)
+        .build();
 
-		ClientSettings clientSettings = ClientSettings.builder()
-				.tokenEndpointAuthenticationSigningAlgorithm(MacAlgorithm.HS256)
-				.build();
+    RegisteredClient client = RegisteredClient.withId(id)
+        .clientId("8000000014")
+        .clientIdIssuedAt(Instant.now())
+        .clientSecret("a5a0ddb27da70b41d31954d0c51419d8")
+        .clientSecretExpiresAt(Instant.now().plus(Period.ofDays(20)))
+        .clientName("Client credentials client_secret_jwt有限公司")
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+        .scope("server")
+        .tokenSettings(tokenSettings)
+        .clientSettings(clientSettings)
+        .build();
+    registeredClientRepository.save(client);
 
-		RegisteredClient client = RegisteredClient.withId(id)
-				.clientId("8000000014")
-				.clientIdIssuedAt(Instant.now())
-				.clientSecret("{noop}secret")
-				.clientSecretExpiresAt(Instant.now().plus(Period.ofDays(20)))
-				.clientName("Client credentials client_secret_jwt有限公司")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.scope("server")
-				.tokenSettings(tokenSettings)
-				.clientSettings(clientSettings)
-				.build();
-		registeredClientRepository.save(client);
-
-		log.info("===>{}", JsonUtils.toJsonString(client));
-	}
+    log.info("===>{}", JsonUtils.toJsonString(client));
+  }
 }
