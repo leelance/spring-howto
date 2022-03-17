913c8fe8f993750168b1713d19bda817c65fe864
lance <li6151770@126.com>
2022-03-17 17:50:02 +0800

    jwt test

--- oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/common/JwtTests.java
+++ oauth2/oauth2-authorization-server/src/test/java/com/lance/oauth2/server/common/JwtTests.java
@@ -6,9 +6,6 @@ import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jose.jwk.source.ImmutableSecret;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +21,9 @@ import org.springframework.util.CollectionUtils;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 /**
@@ -35,31 +34,6 @@ import java.util.function.Predicate;
  */
 @Slf4j
 class JwtTests {
-
-
-  @Test
-  @Disabled
-  void encoder() {
-    //Jwt jwt = Jwt.withTokenValue("33").build();
-    String clientSecret = "abc123456789";
-    SecretKeySpec secretKeySpec = new SecretKeySpec(clientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-    //Jwt jwt = Jwt.withTokenValue(clientSecret).build();
-    JWKSource<SecurityContext> jwkSource = new ImmutableSecret<>(secretKeySpec);
-    NimbusJwtEncoder encoder = new NimbusJwtEncoder(jwkSource);
-    Map<String, Object> claims = new HashMap<>(8);
-    claims.put("secret", clientSecret);
-
-    JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
-    JwtClaimsSet claimsSet = JwtClaimsSet.builder().claims(c -> c.putAll(claims)).build();
-    JwtEncoderParameters parameters = JwtEncoderParameters.from(jwsHeader, claimsSet);
-    Jwt jwtEncode = encoder.encode(parameters);
-    log.info("===>encoder: {}", jwtEncode.getClaims());
-
-    String token = encoder.toString();
-    NimbusJwtDecoder decoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
-    Jwt jwtDecode = decoder.decode(token);
-    log.info("===>decoder: {}", jwtDecode.getClaims());
-  }
 
   @Test
   @Disabled
@@ -128,7 +102,7 @@ class JwtTests {
     signedJWT.sign(signer);
 
     String token = signedJWT.serialize();
-    log.info("===>sign: {}", token);
+    log.info("===>token: {}", token);
 
     NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
     jwtDecoder.setJwtValidator(createJwtValidator(clientId));
