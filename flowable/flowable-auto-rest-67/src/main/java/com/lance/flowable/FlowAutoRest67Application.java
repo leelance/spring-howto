be638bbf44c65abd158590b3f8da1a09494af0b1
lance <li6151770@126.com>
2022-03-30 22:44:44 +0800

    test default rest

--- flowable/flowable-auto-rest-67/src/main/java/com/lance/flowable/FlowAutoRest67Application.java
+++ flowable/flowable-auto-rest-67/src/main/java/com/lance/flowable/FlowAutoRest67Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * aut
+ * flowable auto rest 6.7.2
  *
  * @author lance
  * @date 2022/3/30 12:08
  */
+
 @SpringBootApplication
 public class FlowAutoRest67Application {
 
